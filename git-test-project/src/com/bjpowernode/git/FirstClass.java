235085fdd8ff0c6525687ae83cee87a378387164
zhangsan <zhangsan@tcast.cn>
2020-09-20 23:04:06 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/bjpowernode/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
